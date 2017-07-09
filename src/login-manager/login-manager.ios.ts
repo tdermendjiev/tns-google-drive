18bd44ac13830d7ea5d7bda21539ba06d3465b11
Teodor Dermendzhiev <tdermendjievft@gmail.com>
2017-07-09 22:23:08 +0300

    login-manager class created

--- src/login-manager/login-manager.ios.ts
+++ src/login-manager/login-manager.ios.ts
@@ -1,8 +1,6 @@
 import { GeneralLoginManager } from './login-manager.common';
 export declare class LoginManager extends GeneralLoginManager {
     constructor();
-
-
 }
 
 export function login(callback: Function) {
